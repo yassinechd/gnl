9ebbcdab738432ceb02f9f73eeecb222e9872f74
yassinechd <148255199+yassinechd@users.noreply.github.com>
2024-05-09 05:36:19 +0200

    maj majeure

--- get_next_line.c
+++ get_next_line.c
@@ -1,79 +1,144 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yachadla <yachadla@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/14 21:00:10 by yachadla          #+#    #+#             */
-/*   Updated: 2024/04/23 21:09:45 by yachadla         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+void *ft_free(char *str)
+{
+	if (str)
+		free(str);
+	str = NULL;
+	return (NULL);
+}
 
+char	*ft_reserve(char *reserve, int fd)
+{
+	char *buffer;
+	int byte_read;
+
+	byte_read = 1;
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+
+	if (!buffer)
+	{
+		ft_free(reserve);
+		return (NULL);
+	}
+	while (byte_read > 0)
+	{
+		byte_read = read(fd ,buffer, BUFFER_SIZE);
+		if (byte_read == -1)
+			return (ft_free(buffer), ft_free(reserve));
+		buffer[byte_read] = '\0';
+		if (ft_strchr(buffer, '\n'))
+		{
+			reserve = ft_strjoin(reserve, buffer);
+			break;
+		}
+		if (byte_read == 0)
+		{
+			ft_free(buffer);
+			return (reserve);
+		}
+		reserve = ft_strjoin(reserve, buffer);
+	}
+	ft_free(buffer);
+	return (reserve);
+}
+
+char *ft_result(char *reserve)
+{
+	char	*result;
+	int		i;
+
+	i = 0;
+	if (!reserve[i]) 
+		return (NULL);
+	while (reserve[i] && reserve[i] != '\n')
+		i++;
+	result = malloc(sizeof(char) * (i + 2));
+	if (!result) 
+		return (ft_free(reserve));
+	i = 0;
+	while (reserve[i] && reserve[i] != '\n')
+	{
+		result[i] = reserve[i];
+		i++;
+	}
+	if (reserve[i] == '\n')
+	{
+		result[i] = reserve[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char *ft_new_reserve(char *reserve)
+{
+	int	i;
+	int	j;
+	char *new_reserve;
+	i = 0;
+	if (!reserve[i])
+	{
+		if (reserve)
+			ft_free(reserve);
+		return (NULL);
+	}
+	while (reserve[i] && reserve[i] != '\n')
+		i++;
+	if (!reserve[i])
+		return(ft_free(reserve));
+	new_reserve = malloc(sizeof(char) * (ft_strlen(reserve) - i));
+	if (!new_reserve ) 
+		return (ft_free(reserve));
+	i++;
+	j = 0;
+	while (reserve[i])
+		new_reserve[j++] = reserve[i++];
+	//system("leaks a.out");
+	new_reserve[j] = '\0';
+	ft_free(reserve);
+	return (new_reserve);
+}
 
 char *get_next_line(int fd)
 {
-	char 			*buffer;
-	static char		*reserve;
-	char			*line;
-	int				byte_read;
-	
+    static char	*reserve;
+	char		*line;
+
 	line = NULL;
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
+	if (BUFFER_SIZE < 1 || fd < 0)
 		return (NULL);
-	if (byte_read == -1)
+	reserve = ft_reserve(reserve, fd);
+	if (!reserve)
 		return (NULL);
-	buffer[byte_read] = '\0';
-	byte_read = read(fd, buffer, BUFFER_SIZE);
-	if (ft_strchr(buffer, '\n' )!= 0)
-	{
-		line = ft_decoupe(buffer, reserve, BUFFER_SIZE, ft_strchr(buffer, '\n'));
-		free(buffer);
-		return (line);
-	}
-	while (ft_strchr(buffer, '\n') == 0 && byte_read > 0)
-	{
-		byte_read = read(fd, buffer, BUFFER_SIZE);
-		if (byte_read == -1)
-		return (NULL);
-		buffer[byte_read] = '\0';
-		reserve = ft_strjoin(reserve, buffer);
-	}
-	if (ft_strchr(buffer, '\n' )!= 0)
-	{
-		line = ft_decoupe(buffer, reserve, BUFFER_SIZE, ft_strchr(buffer, '\n'));
-		free(buffer);
-		line = ft_strjoin(reserve, line);
-		return (line);
-	}
-	line = ft_strdup(reserve);
-	free(buffer);
-	free(reserve);
+	line = ft_result(reserve);
+	reserve = ft_new_reserve(reserve);
 	return (line);
-	
 }
 
-int main(void)
-{
-	int fd;
-	char *str;
 
-	fd = open("exemple.txt", O_RDONLY);
-	while (1)
-	{
-		str = get_next_line(fd);
-		if (str == NULL)
-			break ;
-		printf("%s\n", str);
-		str = NULL;
-	}
-	close(fd);
-	return (0);
-}
+
+//int main()
+//{
+//	char *str = NULL;
+//	int fd;
+
+//	fd = open("exemple.txt", O_RDONLY);
+//	while (1)
+//	{
+//		str = get_next_line(fd);
+//		//printf("\nline finale :%s", str);
+//		if (str == NULL)
+//			break;
+//		free(str);
+//		str = NULL;
+//	}
+//	if (str)
+//		free(str);
+
+//	close(fd);
+//	return (0);
+//}
--- get_next_line.h
+++ get_next_line.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yachadla <yachadla@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/14 21:00:17 by yachadla          #+#    #+#             */
-/*   Updated: 2024/04/23 22:11:40 by yachadla         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -17,11 +5,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//# define BUFFER_SIZE 1
+
 char			*get_next_line(int fd);
 size_t			ft_strlen(const	char *str);
-int 	ft_strchr(const char *s, int c);
-char		*ft_strjoin(char const *s1, char const *s2);
-char		*ft_strdup(const char *s);
-char		*ft_decoupe(char *buffer, char **rest, int taille, int pos_n);
+char	*ft_strjoin(char *s1, char *s2);
+char		*ft_strdup(char *s);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+void *ft_free(char *str);
 
 #endif
